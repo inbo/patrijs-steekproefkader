5ed2da99abf297a7eb04eaea69283efbe8dcc6cb
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-10-19 20:25:55 +0200

    use more layers to split sampling framework

--- download.R
+++ download.R
@@ -1,69 +1,188 @@
+library(here)
 library(openssl)
 library(git2rdata)
-dir.create("downloads", showWarnings = FALSE)
+
+options(timeout = max(1000, getOption("timeout")))
+
+dl <- here("downloads")
+dir.create(dl, showWarnings = FALSE)
 
 # jachtterreinen
-target <- "jacht.zip"
-if (!file_test("-f", file.path("downloads", target))) {
-  geopunt <- file.path(
-    "https://downloadagiv.blob.core.windows.net", "jacht", "jachtterr",
-    "2021-2022", "Jacht_2021-2022-01_GewVLA_Shapefile.zip",
-    fsep = "/"
+if (!file_test("-f", here(dl, "jachtter.shp"))) {
+  target <- "jacht.zip"
+  if (!file_test("-f", here(dl, target))) {
+    geopunt <- file.path(
+      "https://downloadagiv.blob.core.windows.net", "jacht", "jachtterr",
+      "2021-2022", "Jacht_2021-2022-01_GewVLA_Shapefile.zip",
+      fsep = "/"
+    )
+    download.file(url = geopunt, here(dl, target))
+  }
+  hash <- sha512(file(here(dl, target)))
+  if (file_test("-f", here(dl, "checksum.tsv"))) {
+    hashes <- read_vc("checksum", dl)
+    stopifnot(
+      "Hash of downloaded file doesn't match the stored hash" =
+        unclass(as.character(hash)) == hashes$sha512[hashes$file == target]
+    )
+  } else {
+    write_vc(
+      data.frame(file = target, sha512 = unclass(as.character(hash))),
+      "checksum", root = dl, sorting = "file", optimize = FALSE
+    )
+  }
+  relevant <- paste0("Jachtter", c(".dbf", ".prj", ".shp", ".shx"))
+  unzip(
+    zipfile = here(dl, target), overwrite = FALSE, junkpaths = TRUE,
+    files = file.path("Shapefile", relevant), setTimes = TRUE, exdir = dl
   )
-  download.file(url = geopunt, file.path("downloads", target))
+  file.rename(here(dl, relevant), here(dl, tolower(relevant)))
 }
-hash <- sha512(file(file.path("downloads", target)))
-if (file_test("-f", file.path("downloads", "checksum.tsv"))) {
-  hashes <- read_vc(file.path("downloads", "checksum"))
-  stopifnot(
-    "Hash of downloaded file doesn't match the stored hash" =
-      unclass(as.character(hash)) == hashes$sha512[hashes$file == target]
-  )
-} else {
-  write_vc(
-    data.frame(file = target, sha512 = unclass(as.character(hash))),
-    file.path("downloads", "checksum"), sorting = "file", optimize = FALSE
-  )
-}
-relevant <- paste0("Jachtter", c(".dbf", ".prj", ".shp", ".shx"))
-unzip(
-  zipfile = file.path("downloads", target), overwrite = FALSE, junkpaths = TRUE,
-  files = file.path("Shapefile", relevant), setTimes = TRUE, exdir = "downloads"
-)
-file.rename(
-  file.path("downloads", relevant), file.path("downloads", tolower(relevant))
-)
 
 # biologische waarderingskaart
-target <- "bwk.zip"
-if (!file_test("-f", file.path("downloads", target))) {
-  geopunt <- file.path(
-    "https://downloadagiv.blob.core.windows.net", "bwk2", "2020",
-    "BWK_en_Natura2000Habitatkaart_2020_GewVLA_Shapefile.zip",
-    fsep = "/"
+if (!file_test("-f", here(dl, "bwkhab.shp"))) {
+  target <- "bwk.zip"
+  if (!file_test("-f", here(dl, target))) {
+    geopunt <- file.path(
+      "https://downloadagiv.blob.core.windows.net", "bwk2", "2020",
+      "BWK_en_Natura2000Habitatkaart_2020_GewVLA_Shapefile.zip",
+      fsep = "/"
+    )
+    download.file(url = geopunt, here(dl, target))
+  }
+  hash <- sha512(file(here(dl, target)))
+  hashes <- read_vc("checksum", dl)
+  if (any(hashes$file == target)) {
+    stopifnot(
+      "Hash of downloaded file doesn't match the stored hash" =
+        unclass(as.character(hash)) == hashes$sha512[hashes$file == target]
+    )
+  } else {
+    write_vc(
+      rbind(
+        data.frame(file = target, sha512 = unclass(as.character(hash))), hashes
+      ),
+      "checksum", root = dl, sorting = "file", optimize = FALSE
+    )
+  }
+  relevant <- paste0("BwkHab", c(".dbf", ".prj", ".shp", ".shx"))
+  unzip(
+    zipfile = here(dl, target), overwrite = FALSE, junkpaths = TRUE,
+    files = file.path("Shapefile", relevant), setTimes = TRUE, exdir = dl
   )
-  download.file(url = geopunt, file.path("downloads", target))
+  file.rename(here(dl, relevant), here(dl, tolower(relevant)))
 }
-hash <- sha512(file(file.path("downloads", target)))
-hashes <- read_vc(file.path("downloads", "checksum"))
-if (any(hashes$file == target)) {
-  stopifnot(
-    "Hash of downloaded file doesn't match the stored hash" =
-      unclass(as.character(hash)) == hashes$sha512[hashes$file == target]
+
+# Vlaanderen
+if (!file_test("-f", here(dl, "refgew.shp"))) {
+  target <- "gemeente.zip"
+  if (!file_test("-f", here(dl, target))) {
+    geopunt <- file.path(
+      "https://downloadagiv.blob.core.windows.net",
+      "referentiebestand-gemeenten",
+      "VoorlopigRefBestandGemeentegrenzen_2019-01-01",
+      "VRBG_toestand_16_05_2018_(geldend_vanaf_01_01_2019)_GewVLA_Shape.zip",
+      fsep = "/"
+    )
+    download.file(url = geopunt, here(dl, target))
+  }
+  hash <- sha512(file(here(dl, target)))
+  hashes <- read_vc("checksum", dl)
+  if (any(hashes$file == target)) {
+    stopifnot(
+      "Hash of downloaded file doesn't match the stored hash" =
+        unclass(as.character(hash)) == hashes$sha512[hashes$file == target]
+    )
+  } else {
+    write_vc(
+      rbind(
+        data.frame(file = target, sha512 = unclass(as.character(hash))), hashes
+      ),
+      "checksum", root = dl, sorting = "file", optimize = FALSE
+    )
+  }
+  bestanden <- unzip(here(dl, "gemeente.zip"), list = TRUE)
+  zippath <- unique(
+    dirname(bestanden$Name[grep("Refgew\\.shp", bestanden$Name)])
   )
-} else {
-  write_vc(
-    rbind(
-      data.frame(file = target, sha512 = unclass(as.character(hash))), hashes
-    ),
-    file.path("downloads", "checksum"), sorting = "file", optimize = FALSE
+  relevant <- paste0("Refgew", c(".dbf", ".prj", ".shp", ".shx"))
+  unzip(
+    zipfile = here(dl, target), overwrite = FALSE, junkpaths = TRUE,
+    files = file.path(zippath, relevant), setTimes = TRUE, exdir = dl
   )
+  file.rename(here(dl, relevant), here(dl, tolower(relevant)))
 }
-relevant <- paste0("BwkHab", c(".dbf", ".prj", ".shp", ".shx"))
-unzip(
-  zipfile = file.path("downloads", target), overwrite = FALSE, junkpaths = TRUE,
-  files = file.path("Shapefile", relevant), setTimes = TRUE, exdir = "downloads"
-)
-file.rename(
-  file.path("downloads", relevant), file.path("downloads", tolower(relevant))
-)
+
+# Vlaamse hydrografische atlas
+if (!file_test("-f", here(dl, "wlas.shp"))) {
+  target <- "waterlopen.zip"
+  if (!file_test("-f", here(dl, target))) {
+    geopunt <- file.path(
+      "https://downloadagiv.blob.core.windows.net",
+      "vlaamse-hydrografische-atlas-waterlopen", "2021",
+      "VHA-waterlopen,%202021-08-28",
+      "VHA_waterlopen_20210828_GewVLA_Shapefile.zip",
+      fsep = "/"
+    )
+    download.file(url = geopunt, here(dl, target))
+  }
+  hash <- sha512(file(here(dl, target)))
+  hashes <- read_vc("checksum", dl)
+  if (any(hashes$file == target)) {
+    stopifnot(
+      "Hash of downloaded file doesn't match the stored hash" =
+        unclass(as.character(hash)) == hashes$sha512[hashes$file == target]
+    )
+  } else {
+    write_vc(
+      rbind(
+        data.frame(file = target, sha512 = unclass(as.character(hash))), hashes
+      ),
+      "checksum", root = dl, sorting = "file", optimize = FALSE
+    )
+  }
+  relevant <- paste0("Wlas", c(".dbf", ".prj", ".shp", ".shx"))
+  unzip(
+    zipfile = here(dl, target), overwrite = FALSE, junkpaths = TRUE,
+    files = file.path("Shapefile", relevant), setTimes = TRUE, exdir = dl
+  )
+  file.rename(here(dl, relevant), here(dl, tolower(relevant)))
+}
+
+# Wegenregister
+if (!file_test("-f", here(dl, "refgew.shp"))) {
+  target <- "wegenregister.zip"
+  if (!file_test("-f", here(dl, target))) {
+    geopunt <- file.path(
+      "https://downloadagiv.blob.core.windows.net", "wegenregister",
+      "Wegenregister_SHAPE_20210916.zip",
+      fsep = "/"
+    )
+    download.file(url = geopunt, here(dl, target))
+  }
+  hash <- sha512(file(here(dl, target)))
+  hashes <- read_vc("checksum", dl)
+  if (any(hashes$file == target)) {
+    stopifnot(
+      "Hash of downloaded file doesn't match the stored hash" =
+        unclass(as.character(hash)) == hashes$sha512[hashes$file == target]
+    )
+  } else {
+    write_vc(
+      rbind(
+        data.frame(file = target, sha512 = unclass(as.character(hash))), hashes
+      ),
+      "checksum", root = dl, sorting = "file", optimize = FALSE
+    )
+  }
+  bestanden <- unzip(here(dl, "wegenregister.zip"), list = TRUE)
+  zippath <- unique(
+    dirname(bestanden$Name[grep("Wegsegment\\.shp", bestanden$Name)])
+  )
+  relevant <- paste0("Wegsegment", c(".dbf", ".prj", ".shp", ".shx"))
+  unzip(
+    zipfile = here(dl, target), overwrite = FALSE, junkpaths = TRUE,
+    files = file.path(zippath, relevant), setTimes = TRUE, exdir = dl
+  )
+  file.rename(here(dl, relevant), here(dl, tolower(relevant)))
+}
--- steekproefkader.R
+++ steekproefkader.R
@@ -11,7 +11,6 @@ jachtgebieden <- here("downloads", "jachtter.shp")
 vlaanderen <- here("downloads", "refgew.shp")
 wegen <- here("downloads", "wegsegment.shp")
 waterlopen <- here("downloads", "wlas.shp")
-spoorwegen <- here("downloads", "spoorweg.shp")
 
 if (!file.exists(file.path(target_folder, "open_ruimte.gpkg"))) {
   read_vc("bwk_eenheid", root = here("downloads")) %>%
@@ -169,4 +168,238 @@ if (!file.exists(file.path(target_folder, "kader_01.gpkg"))) {
       )
     ) %>%
     do.call(what = qgis_run_algorithm)
+}
+
+if (!file.exists(file.path(target_folder, "level_01.gpkg"))) {
+  wegen %>%
+    read_sf() %>%
+    filter(LBLWEGCAT %in% c("hoofdweg", "primaire weg I")) %>%
+    st_transform(crs = 31370) %>%
+    select(LBLWEGCAT) -> hoofdweg
+  waterlopen %>%
+    read_sf() %>%
+    filter(LBLCATC %in% c("Bevaarbaar")) %>%
+    st_transform(crs = 31370) %>%
+    select(LBLCATC) -> bevaarbaar
+  vlaanderen %>%
+    read_sf() %>%
+    st_transform(crs = 31370) %>%
+    st_write(
+      dsn = file.path(target_folder, "vlaanderen.shp"),
+      delete_dsn = file.exists(
+        file.path(target_folder, "vlaanderen.shp")
+      )
+    )
+  qgis_run_algorithm(
+    "native:buffer", INPUT = bevaarbaar, DISTANCE = 20, DISSOLVE = TRUE,
+    END_CAP_STYLE = 0, JOIN_STYLE = 0, MITER_LIMIT = 2, SEGMENTS = 5
+  ) %>%
+    qgis_output("OUTPUT") %>%
+    as.character() -> bevaarbaar
+  qgis_run_algorithm(
+    "native:buffer", INPUT = hoofdweg, DISTANCE = 20, DISSOLVE = TRUE,
+    END_CAP_STYLE = 0, JOIN_STYLE = 0, MITER_LIMIT = 2, SEGMENTS = 5
+  ) %>%
+    qgis_output("OUTPUT") %>%
+    setNames("OVERLAY") %>%
+    c(
+      list(
+        algorithm = "native:difference",
+        INPUT = file.path(target_folder, "vlaanderen.shp")
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    setNames("INPUT") %>%
+    c(list(algorithm = "native:difference", OVERLAY = bevaarbaar)) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    setNames("INPUT") %>%
+    c(list(algorithm = "native:multiparttosingleparts")) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:buffer", DISTANCE = 19.8, DISSOLVE = FALSE,
+        END_CAP_STYLE = 0, JOIN_STYLE = 0, MITER_LIMIT = 2, SEGMENTS = 5
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    read_sf() %>%
+    mutate(level1 = row_number()) %>%
+    select(-OIDN) %>%
+    st_write(
+      dsn = file.path(target_folder, "level_01.gpkg"),
+      delete_dsn = file.exists(
+        file.path(target_folder, "level_01.gpkg")
+      )
+    )
+}
+
+refine_level <- function(
+    current_level, target_folder, split_laag, split_var, split_val
+) {
+  # selecteer nieuwe grenzen
+  qgis_run_algorithm(
+    "native:extractbyexpression",
+    INPUT = split_laag,
+    EXPRESSION = paste0(split_var, " = '", split_val, "'", collapse = " OR ")
+  ) %>%
+    qgis_output("OUTPUT") %>%
+    unclass() -> split_laag
+  # selecteer te verkleinen polygonen van steekproefkader
+  qgis_run_algorithm(
+    "native:extractbyexpression",
+    INPUT = sprintf("%s/kader_%02i.gpkg", target_folder, current_level),
+    EXPRESSION =
+      "ha > 150 OR te_breed = 1 OR te_hoog = 1 OR (ratio < 0.1 AND ha > 1)"
+  ) %>%
+    qgis_output("OUTPUT") %>%
+    # selecteer de overeenkomstige eiland polygonen
+    setNames("INTERSECT") %>%
+    c(
+      list(
+        algorithm = "native:extractbylocation",
+        INPUT = sprintf("%s/level_%02i.gpkg", target_folder, current_level)
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    unclass() -> level_selectie
+  # snijd de beschikbare grenzen bij aan de hand van de geselecteerde eiland
+  # polygonen
+  qgis_run_algorithm(
+    "native:clip", INPUT = split_laag, OVERLAY = level_selectie
+  ) %>%
+    qgis_output("OUTPUT") %>%
+    # buffer de overgebleven grenzen
+    setNames("INPUT") %>%
+    c(list(algorithm = "native:buffer", DISTANCE = 10)) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # knip de buffers uit de eiland polygonen
+    setNames("OVERLAY") %>%
+    c(
+      list(
+        algorithm = "native:difference",
+        INPUT = sprintf("%s/level_%02i.gpkg", target_folder, current_level)
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # split de nieuwe eilandpolygonen in afzonderlijke polygonen
+    setNames("INPUT") %>%
+    c(list(algorithm = "native:multiparttosingleparts")) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # herstel buffer
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:buffer", DISTANCE = 9.8, DISSOLVE = FALSE,
+        END_CAP_STYLE = 0, JOIN_STYLE = 0, MITER_LIMIT = 2, SEGMENTS = 5
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # voeg een nieuwe id toe aan de eilandpolygonen
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:addautoincrementalfield",
+        FIELD_NAME = paste0("level", current_level + 1),
+        OUTPUT = sprintf("%s/level_%02i.gpkg", target_folder, current_level + 1)
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # versnij de eilandpolygonen met het basissteekproefkader
+    setNames("OVERLAY") %>%
+    c(
+      list(
+        algorithm = "native:intersection",
+        INPUT = file.path(target_folder, "kader_01.gpkg"),
+        INPUT_FIELDS = c("wbe", "veld", "open_id"),
+        OVERLAY_FIELDS = paste0("level", seq_len(current_level + 1)),
+        OVERLAY_FIELDS_PREFIX = ""
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # zet het steekproefkader om naar enkelvoudige polygonen
+    setNames("INPUT") %>%
+    c(list(algorithm = "native:multiparttosingleparts")) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # bereken de breedte van de bounding box in hm
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:fieldcalculator", FIELD_NAME = "dx",
+        FORMULA = "bounds_width($geometry) / 100"
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # bereken de hoogte van de bounding box in hm
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:fieldcalculator", FIELD_NAME = "dy",
+        FORMULA = "bounds_height($geometry) / 100"
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # bereken de oppervlakte in ha
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:fieldcalculator", FIELD_NAME = "ha",
+        FORMULA = "$area / 10000"
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # bepaal polygonen breder of hoger dan 27 hm
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:fieldcalculator", FIELD_NAME = "te_breed",
+        FORMULA = "if(if( dx > dy,dx, dy) > 27, 1, 0)", FIELD_TYPE = 1
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # bepaal polygonen breder of hoger dan 19 hm
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:fieldcalculator", FIELD_NAME = "te_hoog",
+        FORMULA = "if(if( dx > dy, dy, dx) > 19, 1, 0)", FIELD_TYPE = 1
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT") %>%
+    # bereken de verhouding oppervlakte / bounding box
+    setNames("INPUT") %>%
+    c(
+      list(
+        algorithm = "native:fieldcalculator", FIELD_NAME = "ratio",
+        FORMULA = "ha / dx / dy",
+        OUTPUT = sprintf("%s/kader_%02i.gpkg", target_folder, current_level + 1)
+      )
+    ) %>%
+    do.call(what = qgis_run_algorithm) %>%
+    qgis_output("OUTPUT")
+}
+
+message("kader 2")
+if (!file.exists(file.path(target_folder, "kader_02.gpkg"))) {
+  refine_level(
+    current_level = 1, split_laag = waterlopen, split_var = "LBLCATC",
+    split_val = "Geklasseerd, eerste categorie", target_folder = target_folder
+  )
 }
