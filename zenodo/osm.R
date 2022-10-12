80a29ec333b9683c93ff148a2dd9ffd6ad9f093a
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-10-12 16:28:30 +0200

    upload new version of OSM

--- zenodo/osm.R
+++ zenodo/osm.R
@@ -8,28 +8,11 @@ osm_pbf <- oe_download(
   file_url = osm_source$url, file_size = osm_source$file_size
 )
 
-zenodo <- ZenodoManager$new(token = key_get("zenodo"))
-
-myrec <- ZenodoRecord$new()
-myrec$setTitle("OpenStreetMap Data Extracts for Belgium")
-myrec$setDescription(
-  paste(
-    "Geofabrik provides data extracts from the OpenStreetMap project.",
-    "This is a copy of the data from",
-    "https://download.geofabrik.de/europe/belgium.html."
-  )
+zenodo <- ZenodoManager$new(token = key_get("zenodo"), logger = "INFO")
+myrec <- zenodo$getDepositionById("5792949")
+myrec <- zenodo$depositRecordVersion(
+  myrec, delete_latest_files = TRUE, files = osm_pbf, publish = FALSE
 )
-myrec$setUploadType("dataset")
-myrec$addCreator(lastname = "OpenStreetMap Contributors", firstname = "")
-myrec$addCreator(lastname = "Geofabrik", firstname = "")
-myrec$setLicense("ODbL-1.0")
-myrec$setAccessRight("open")
-myrec$setPublicationDate(as.Date("2021-12-20"))
-myrec$addRelatedIdentifier(
-  "isCompiledBy", "https://download.geofabrik.de/europe/belgium.html"
-)
-myrec <- zenodo$depositRecord(myrec)
-undebug(zenodo$uploadFile)
-zenodo$uploadFile(osm_pbf, myrec)
-
+myrec$setPublicationDate(Sys.Date())
+myrec$setVersion(Sys.Date())
 myrec <- zenodo$depositRecord(myrec, publish = TRUE)
