740aa124feb69fcc31b6c95f7e891a681f7df18b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-10-12 18:42:05 +0200

    update hunting grounds

--- zenodo/update_jacht.R
+++ zenodo/update_jacht.R
@@ -6,16 +6,17 @@ library(curl)
 library(tidyverse)
 target <- here("zenodo")
 paste(
-  "https://downloadagiv.blob.core.windows.net/jacht/jachtterr/2021-2022",
-  "Jacht_2021-2022-02_GewVLA_Shapefile.zip",
+  "https://downloadagiv.blob.core.windows.net/jacht/jachtterr/2022-2023",
+  "Jacht_Shapefile.zip",
   sep = "/"
 ) %>%
   curl_download(here(target, "jacht.zip"))
 
-zenodo <- ZenodoManager$new(token = key_get("zenodo"))
-myrec <- zenodo$getDepositionByDOI(
-  paste("10.5281", "zenodo.5584204", sep = "/")
-)
+zenodo <- ZenodoManager$new(token = key_get("zenodo"), logger = "INFO")
+myrec <- zenodo$getDepositionById("5584204")
 myrec <- zenodo$depositRecordVersion(
-  myrec, delete_latest_files = TRUE, here(target, "jacht.zip"), publish = TRUE
+  myrec, delete_latest_files = TRUE, here(target, "jacht.zip")
 )
+myrec$setPublicationDate(as.Date("2022-08-24"))
+myrec$setVersion("seizoen 2022-2023.01")
+myrec <- zenodo$depositRecord(myrec, publish = TRUE)
