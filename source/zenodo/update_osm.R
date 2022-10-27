9eac0278085fe00606b9e85d97eed92c238e5fc0
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-10-27 21:04:59 +0200

    update fieldmaps with openstreetmap background

---
+++ source/zenodo/update_osm.R
@@ -0,0 +1,98 @@
+renv::restore()
+library(here)
+library(keyring)
+library(zen4R)
+library(tidyverse)
+source_folder <- here("data", "fieldmap", "osm")
+zenodo <- ZenodoManager$new(token = key_get("zenodo"), logger = "INFO")
+
+new_version <- "2023.01"
+doi <- c(
+  vlbr = 5815210, limb = 5815380, antw = 5815393, oovl = 5815401,
+  wevl = 5815408
+)
+
+current <- "wevl"
+myrec <- zenodo$getDepositionById(doi[current])
+myrec <- zenodo$depositRecordVersion(
+  record = myrec, delete_latest_files = TRUE, publish = FALSE
+)
+myrec$setPublicationDate(Sys.Date())
+myrec$setVersion(new_version)
+source_folder %>%
+  file.path(current) %>%
+  list.files(full.names = TRUE) -> to_do
+walk(to_do, zenodo$uploadFile, record = myrec)
+zenodo$getFiles(myrec$id) %>%
+  map_chr("filename") -> done
+to_do <- to_do[!basename(to_do) %in% done]
+stopifnot(length(to_do) == 0)
+myrec <- zenodo$depositRecord(myrec, publish = TRUE)
+
+current <- "oovl"
+myrec <- zenodo$getDepositionById(doi[current])
+myrec <- zenodo$depositRecordVersion(
+  record = myrec, delete_latest_files = TRUE, publish = FALSE
+)
+myrec$setPublicationDate(Sys.Date())
+myrec$setVersion(new_version)
+source_folder %>%
+  file.path(current) %>%
+  list.files(full.names = TRUE) -> to_do
+walk(to_do, zenodo$uploadFile, record = myrec)
+zenodo$getFiles(myrec$id) %>%
+  map_chr("filename") -> done
+to_do <- to_do[!basename(to_do) %in% done]
+stopifnot(length(to_do) == 0)
+myrec <- zenodo$depositRecord(myrec, publish = TRUE)
+
+current <- "antw"
+myrec <- zenodo$getDepositionById(doi[current])
+myrec <- zenodo$depositRecordVersion(
+  record = myrec, delete_latest_files = TRUE, publish = FALSE
+)
+myrec$setPublicationDate(Sys.Date())
+myrec$setVersion(new_version)
+source_folder %>%
+  file.path(current) %>%
+  list.files(full.names = TRUE) -> to_do
+walk(to_do, zenodo$uploadFile, record = myrec)
+zenodo$getFiles(myrec$id) %>%
+  map_chr("filename") -> done
+to_do <- to_do[!basename(to_do) %in% done]
+stopifnot(length(to_do) == 0)
+myrec <- zenodo$depositRecord(myrec, publish = TRUE)
+
+current <- "limb"
+myrec <- zenodo$getDepositionById(doi[current])
+myrec <- zenodo$depositRecordVersion(
+  record = myrec, delete_latest_files = TRUE, publish = FALSE
+)
+myrec$setPublicationDate(Sys.Date())
+myrec$setVersion(new_version)
+source_folder %>%
+  file.path(current) %>%
+  list.files(full.names = TRUE) -> to_do
+walk(to_do, zenodo$uploadFile, record = myrec)
+zenodo$getFiles(myrec$id) %>%
+  map_chr("filename") -> done
+to_do <- to_do[!basename(to_do) %in% done]
+stopifnot(length(to_do) == 0)
+myrec <- zenodo$depositRecord(myrec, publish = TRUE)
+
+current <- "vlbr"
+myrec <- zenodo$getDepositionById(doi[current])
+myrec <- zenodo$depositRecordVersion(
+  record = myrec, delete_latest_files = TRUE, publish = FALSE
+)
+myrec$setPublicationDate(Sys.Date())
+myrec$setVersion(new_version)
+source_folder %>%
+  file.path(current) %>%
+  list.files(full.names = TRUE) -> to_do
+walk(to_do, zenodo$uploadFile, record = myrec)
+zenodo$getFiles(myrec$id) %>%
+  map_chr("filename") -> done
+to_do <- to_do[!basename(to_do) %in% done]
+stopifnot(length(to_do) == 0)
+myrec <- zenodo$depositRecord(myrec, publish = TRUE)
