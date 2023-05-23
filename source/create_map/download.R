825ad29bf23deda92c362a5e10810e0c4ee4df59
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-05-23 16:33:19 +0200

    update OSM data

--- source/create_map/download.R
+++ source/create_map/download.R
@@ -14,10 +14,10 @@ dir.create(dl, showWarnings = FALSE)
 if (file_test("-f", here(dl, "jachtter.shp"))) {
   relevant <- paste0("jachtter", c(".dbf", ".prj", ".shp", ".shx"))
   hashes <- read_vc("checksum", dl)
-  map(here(dl, relevant), file) %>%
-    map(sha512) %>%
+  map(here(dl, relevant), file) |>
+    map(sha512) |>
     map_chr(as.character) -> hash
-  data.frame(file = relevant, check = hash) %>%
+  data.frame(file = relevant, check = hash) |>
     inner_join(hashes, by = "file") -> to_test
   stopifnot(
     "Hash mismatch" = nrow(to_test) == length(relevant),
@@ -27,12 +27,12 @@ if (file_test("-f", here(dl, "jachtter.shp"))) {
 } else {
   target <- "jacht.zip"
   if (!file_test("-f", here(dl, target))) {
-    download_zenodo(doi = "7190764", path = dl)
+    download_zenodo(doi = "10.5281/zenodo.7190764", path = dl, timeout = 600)
   }
-  here(dl, target) %>%
-    file() %>%
-    sha512() %>%
-    as.character() %>%
+  here(dl, target) |>
+    file() |>
+    sha512() |>
+    as.character() |>
     unclass() -> hash
   if (file_test("-f", here(dl, "checksum.csv"))) {
     hashes <- read_vc("checksum", dl)
@@ -53,10 +53,10 @@ if (file_test("-f", here(dl, "jachtter.shp"))) {
     files = file.path("Shapefile", relevant), setTimes = TRUE, exdir = dl
   )
   file.rename(here(dl, relevant), here(dl, tolower(relevant)))
-  map(here(dl, tolower(relevant)), file) %>%
-    map(sha512) %>%
+  map(here(dl, tolower(relevant)), file) |>
+    map(sha512) |>
     map_chr(as.character) -> hash
-  data.frame(file = tolower(relevant), check = hash) %>%
+  data.frame(file = tolower(relevant), check = hash) |>
     inner_join(hashes, by = "file") -> to_test
   stopifnot(
     "Hash of downloaded file doesn't match the stored hash" = all(
@@ -64,31 +64,31 @@ if (file_test("-f", here(dl, "jachtter.shp"))) {
         all(to_test$check == to_test$sha512)
     )
   )
-  data.frame(file = tolower(relevant), check = hash) %>%
-    anti_join(hashes, by = "file") %>%
-    bind_rows(hashes) %>%
-    write_vc("checksum", root = dl, optimized = FALSE)
+  data.frame(file = tolower(relevant), sha512 = hash) |>
+    anti_join(hashes, by = "file") |>
+    bind_rows(hashes) |>
+    write_vc("checksum", root = dl, optimize = FALSE)
 }
 
 # OpenStreetMap
 target <- "geofabrik_belgium-latest.osm.pbf"
 hashes <- read_vc("checksum", dl)
 if (file_test("-f", here(dl, target))) {
-  here(dl, target) %>%
-    file() %>%
-    sha512() %>%
-    as.character() %>%
+  here(dl, target) |>
+    file() |>
+    sha512() |>
+    as.character() |>
     unclass() -> hash
   stopifnot(
     "Hash of downloaded file doesn't match the stored hash" =
       hash == hashes$sha512[hashes$file == target]
   )
 } else {
-  download_zenodo(doi = "6628312", path = dl)
-  here(dl, target) %>%
-    file() %>%
-    sha512() %>%
-    as.character() %>%
+  download_zenodo(doi = "10.5281/zenodo.7962764", path = dl, timeout = 600)
+  here(dl, target) |>
+    file() |>
+    sha512() |>
+    as.character() |>
     unclass() -> hash
   if (any(hashes$file == target)) {
     stopifnot(
