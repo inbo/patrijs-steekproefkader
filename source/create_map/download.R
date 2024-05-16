af8a670ca9639b006d9792d9049281c1257d3d60
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-05-16 15:47:15 +0200

    v 2024.1

--- source/create_map/download.R
+++ source/create_map/download.R
@@ -27,7 +27,7 @@ if (file_test("-f", here(dl, "jachtter.shp"))) {
 } else {
   target <- "jacht.zip"
   if (!file_test("-f", here(dl, target))) {
-    download_zenodo(doi = "10.5281/zenodo.7190764", path = dl, timeout = 600)
+    download_zenodo(doi = "10.5281/zenodo.10212759", path = dl, timeout = 600)
   }
   here(dl, target) |>
     file() |>
@@ -84,7 +84,7 @@ if (file_test("-f", here(dl, target))) {
       hash == hashes$sha512[hashes$file == target]
   )
 } else {
-  download_zenodo(doi = "10.5281/zenodo.7962764", path = dl, timeout = 600)
+  download_zenodo(doi = "10.5281/zenodo.10212939", path = dl, timeout = 600)
   here(dl, target) |>
     file() |>
     sha512() |>
