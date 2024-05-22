444887e5753733c7a0eda73cab67fe12ce1b9b9b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-05-22 17:21:16 +0200

    use new version of qgisprocess for hunting grounds

--- source/create_map/hunting_grounds.R
+++ source/create_map/hunting_grounds.R
@@ -10,33 +10,21 @@ dir.create(target_folder, showWarnings = FALSE)
 
 source(here("source", "create_map", "download.R"))
 
-here(download_folder, "jachtter.shp") %>%
-  setNames("INPUT") %>%
-  c(list(
+here(download_folder, "jachtter.shp") |>
+  setNames("INPUT") |>
+  qgis_run_algorithm_p(
     algorithm = "native:buffer", DISTANCE = 0, DISSOLVE = FALSE,
     END_CAP_STYLE = 0, JOIN_STYLE = 0, MITER_LIMIT = 2, SEGMENTS = 5,
-    OUTPUT = qgis_tmp_vector()
-  )) %>%
-  do.call(what = qgis_run_algorithm) %>%
-  qgis_output("OUTPUT") %>%
-  setNames("INPUT") %>%
-  c(list(
+    OUTPUT = qgis_tmp_vector(), SEPARATE_DISJOINT = FALSE
+  ) |>
+  qgis_run_algorithm_p(
     algorithm = "native:dissolve", FIELD = c("VELDID", "WBENR"),
-    OUTPUT = qgis_tmp_vector()
-  )) %>%
-  do.call(what = qgis_run_algorithm) %>%
-  qgis_output("OUTPUT") %>%
-  setNames("INPUT") %>%
-  c(list(
+    OUTPUT = qgis_tmp_vector(), SEPARATE_DISJOINT = FALSE
+  ) |>
+  qgis_run_algorithm_p(
     algorithm = "native:multiparttosingleparts", OUTPUT = qgis_tmp_vector()
-  )) %>%
-  do.call(what = qgis_run_algorithm) %>%
-  qgis_output("OUTPUT") %>%
-  setNames("INPUT") %>%
-  c(list(
-    algorithm = "native:reprojectlayer",
-    OUTPUT = here(target_folder, "jacht.gpkg"),
-    TARGET_CRS = "PROJ4:+proj=longlat +datum=WGS84 +no_defs"
-  )) %>%
-  do.call(what = qgis_run_algorithm)
-qgis_tmp_clean()
+  ) |>
+  qgis_run_algorithm_p(
+    algorithm = "native:reprojectlayer", TARGET_CRS = "EPSG:4326",
+    OUTPUT = here(target_folder, "jacht.gpkg")
+  )
