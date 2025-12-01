1c6483e2625b8358a7bffaf36e73977296a946ec
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-12-01 11:00:30 +0100

    ✨ Upload open area map version 2026.01

--- source/zenodo/update_open_area.R
+++ source/zenodo/update_open_area.R
@@ -7,9 +7,6 @@ library(zen4R)
 here("data", "open_area", "open_ruimte.gpkg") |>
   read_sf() |>
   write_sf(dsn = here("data", "open_area", "open_ruimte.shp"))
-here("data", "sampling", "telblok.gpkg") |>
-  read_sf() |>
-  write_sf(dsn = here("data", "sampling", "telblok.shp"))
 
 zenodo <- ZenodoManager$new(token = key_get("zenodo"), logger = "INFO")
 myrec <- zenodo$getDepositionByDOI("10.5281/zenodo.10260759")
@@ -24,20 +21,5 @@ here("data", "open_area") |>
     publish = FALSE
   ) -> myrec
 myrec$setPublicationDate(Sys.Date())
-myrec$setVersion("2025.06")
-myrec <- zenodo$depositRecord(myrec, publish = TRUE)
-
-myrec <- zenodo$getDepositionByDOI("10.5281/zenodo.5814901")
-here("data", "sampling") |>
-  list.files(
-    pattern = "^telblok\\.(gpkg|dbf|prj|shp|shx)$",
-    full.names = TRUE
-  ) |>
-  zenodo$depositRecordVersion(
-    record = myrec,
-    delete_latest_files = TRUE,
-    publish = FALSE
-  ) -> myrec
-myrec$setPublicationDate(Sys.Date())
-myrec$setVersion("2025.06")
+myrec$setVersion("2026.01")
 myrec <- zenodo$depositRecord(myrec, publish = TRUE)
